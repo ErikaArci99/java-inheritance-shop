f51b3e20bbb635db5ac34e92c810492ab33eb062
ErikaArci99 <erikaarci1999@outlook.it>
2025-07-30 18:33:26 +0200

    added setter and getter in products

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -17,4 +17,43 @@ public class Prodotto {
         this.prezzo = prezzo;
         this.iva = iva;
     }
+
+    // prende il codice solo in lettura
+    public int getCodice() {
+        return codice;
+    }
+
+    // gli altri attributi sia in lettura chre in scrittura
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
 }
