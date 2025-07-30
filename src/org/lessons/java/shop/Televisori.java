10af2b4c6034469975d8aa69c0b5e24a102c759d
ErikaArci99 <erikaarci1999@outlook.it>
2025-07-30 18:46:36 +0200

    Added television class with costructor and get and set

--- src/org/lessons/java/shop/Televisori.java
+++ src/org/lessons/java/shop/Televisori.java
@@ -1,5 +1,33 @@
 package org.lessons.java.shop;
 
-public class Televisori {
+public class Televisori extends Prodotto {
 
+    // Attributi specifici
+    private int dimensione;
+    private boolean smartTv;
+
+    // Costruttore
+    public Televisori(int codice, String nome, String marca, double prezzo, double iva, int dimensione,
+            boolean smartTv) {
+        super(codice, nome, marca, prezzo, iva);
+        this.dimensione = dimensione;
+        this.smartTv = smartTv;
+    }
+
+    // Metodi getter e setter
+    public int getDimensione() {
+        return dimensione;
+    }
+
+    public void setDimensione(int dimensione) {
+        this.dimensione = dimensione;
+    }
+
+    public boolean isSmartTv() {
+        return smartTv;
+    }
+
+    public void setSmartTv(boolean smartTv) {
+        this.smartTv = smartTv;
+    }
 }
